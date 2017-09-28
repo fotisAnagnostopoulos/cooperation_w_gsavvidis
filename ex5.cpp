d52d24eceb9deedc2ea539c9671de687aa92d797
fotis <fotis-anagnostopoulos@hotmail.com>
2017-09-28 22:28:49 +0300

    an error regarding the calculation of velocities had been corrected. We also need to test it extensively

--- ex5.cpp
+++ ex5.cpp
@@ -5,12 +5,13 @@
 using namespace std;
 
 double force(double x_01, double y_01, double x_02, double y_02);
-double verlet_alg(double x_01, double y_01, double x_02, double y_02, double ux_02, double uy_02, double t);
+double verlet_alg(double x_01, double y_01, double x_02, double y_02, double ux_02, double t);
+double velocity_verlet(double ux_01, double force_old,double force_new, double t);
 
 int main()
 {
-	int t_steps = 50;
-	double t = 10.;
+	int t_steps = 100;
+	double t = 1.;
 	struct point_data
 	 {
 	 	int id;
@@ -22,16 +23,16 @@ int main()
 
 	 //initial_conditions for both point masses
 	 point_1.id = 1;
-	 point_1.x_position.push_back(4.);
-	 point_1.y_position.push_back(2.);
+	 point_1.x_position.push_back(4.); //4.  is the correct value
+	 point_1.y_position.push_back(2.); //2.
 	 point_1.x_velocity.push_back(1.);
 	 point_1.y_velocity.push_back(-2.);
 
 	 point_2.id = 2;
 	 point_2.x_velocity.push_back(1.);
 	 point_2.y_velocity.push_back(-2.);
-	 point_2.x_position.push_back(3.);
-	 point_2.y_position.push_back(1.);
+	 point_2.x_position.push_back(3.); //3
+	 point_2.y_position.push_back(1.);//1
 
 	 //calculating loop
 	 double dt = t/t_steps;
@@ -51,32 +52,39 @@ int main()
 	 	double ux_02 = point_2.x_velocity[i];
 	 	double uy_02 = point_2.y_velocity[i];
 
+	 	double force_old_12 = force(x_01,y_01,x_02,y_02);
+	 	double force_old_21 = force(x_02,y_02,x_01,y_01);
+
 	 	//Initially the first point mass stays at rest and the second one moves. Its new position:
-	 	double x_2new = verlet_alg(x_01, y_01, x_02, y_02, ux_02, uy_02, dt);
+	 	double x_2new = verlet_alg(x_01, y_01, x_02, y_02, ux_02, dt);
 	 	point_2.x_position.push_back(x_2new);
 	 	//for both coordinates (just interchanging x <--> y):
-	 	double y_2new = verlet_alg(y_01, x_01, y_02, x_02, uy_02, ux_02, dt);
+	 	double y_2new = verlet_alg(y_01, x_01, y_02, x_02, uy_02, dt);
 	 	point_2.y_position.push_back(y_2new);
 
-	 	//Now, the velocities (if i understand correctly the verlet alg., we suppose that in certain small enough dt's the particle moves with constant force ara kai constant acceleration:
-	 	double ux_2new = force(x_01,y_01,x_02,y_02) * dt;
-	 	point_2.x_velocity.push_back(ux_2new);
-	 	//similar with the displacement (i hope)
-	 	double uy_2new = force(y_01,x_01,y_02,x_02) * dt;
-	 	point_2.y_velocity.push_back(uy_2new);
-	 	
+
 	 	//Secondly, the poin mass with id = 2 stays at rest and the first one moves, so we have to set x_01 --> x_02 and also y_01 --> y_02
-	 	
-	 	double x_1new = verlet_alg(x_02, y_02, x_01, y_01, ux_01, uy_01, dt);
+	 	double x_1new = verlet_alg(x_02, y_02, x_01, y_01, ux_01, dt);
 	 	point_1.x_position.push_back(x_1new);
 	 	////Similar to the above
-	 	double y_1new = verlet_alg(y_02, x_02, y_01, x_01, uy_01, ux_01, dt);
+	 	double y_1new = verlet_alg(y_02, x_02, y_01, x_01, uy_01, dt);
 	 	point_1.y_position.push_back(y_1new);
 	 	/////
-	 	double ux_1new = force(x_02,y_02,x_01,y_01) * dt;
+
+	 	double force_new_12 = force(x_1new,y_1new,x_2new,y_2new);
+	 	double force_new_21 = force(x_2new, y_2new, x_1new,y_1new);
+
+	 	//Now, the velocities 
+	 	double ux_2new = velocity_verlet(ux_02,force_old_12,force_new_12,dt);
+	 	point_2.x_velocity.push_back(ux_2new);
+	 	//similar with the displacement (i hope)
+	 	double uy_2new = velocity_verlet(uy_02,force_old_12,force_new_12,dt);
+	 	point_2.y_velocity.push_back(uy_2new);
+
+	 	double ux_1new = velocity_verlet(ux_01,force_old_21,force_new_21,dt);
 	 	point_1.x_velocity.push_back(ux_1new);
 	 	//similar with the displacement (i hope)
-	 	double uy_1new = force(y_02,x_02,y_01,x_01) * dt;
+	 	double uy_1new =  velocity_verlet(uy_01,force_old_21,force_new_21,dt);
 	 	point_2.y_velocity.push_back(uy_1new);
 
 	 	///All these additional variables are not needed but it is more readable that way. This must be reconsidered.
@@ -86,7 +94,9 @@ int main()
 	 // Printing loop
 	 for (int i = 0; i <  point_1.x_position.size(); ++i)
 	 {
-	 	cout << point_1.x_position[i] <<"," << endl;
+	 	// cout << point_1.x_position[i] <<"," << endl;
+	 	// cout << pow(pow(point_1.x_position[i],2.) + pow(point_1.x_position[i],2.),0.5) <<"," << endl;
+	 	cout << pow(pow(point_2.y_position[i]-point_1.y_position[i],2.) + pow(point_2.x_position[i]-point_1.x_position[i],2.),0.5) <<"," << endl;
 	 }
 	 
 
@@ -101,7 +111,7 @@ double force(double x_01, double y_01, double x_02, double y_02){
 	return forc;
 }
 
-double verlet_alg(double x_01, double y_01, double x_02, double y_02, double ux_02, double uy_02, double t){
+double verlet_alg(double x_01, double y_01, double x_02, double y_02, double ux_02, double t){
 	// This function takes as arguments the vectors (with the physical meaning) x1,x2,u1 at time t and returns the x-part of theposition of the particle at time t+dt.
 	// in order to calculate the y-part of displacement, we just need to interchange x with y. This is only true for the lenard - jones potential due to spatial symetry. (see the python notebook
 	//for the proof)
@@ -110,4 +120,11 @@ double verlet_alg(double x_01, double y_01, double x_02, double y_02, double ux_
 	double x_new = x_02 + ux_02 * t + 1./2. * a_x * pow(t,2.);
 	return x_new;
 
+}
+
+double velocity_verlet(double ux_02, double force_old,double force_new, double t){
+	double vel;
+	vel = ux_02 + (force_old + force_new)/2. * t;
+
+	return vel;
 }
