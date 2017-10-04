75257895bb18b1e301bdeb11a321d4ac8cc55912
fotisAnagnostopoulos <fotis-anagnostopoulos@hotmail.com>
2017-10-04 15:16:00 +0300

    i fixed the issue giorgare

--- ex3.cpp
+++ ex3.cpp
@@ -11,20 +11,24 @@ int main(int argc, char const *argv[])
 {
 	srand(time(NULL));
 	int N = 100000;
-	int node_num[100000] = {0};
+	int node_num[N];
 	double gamma = 2.;
 	int i = 0,j = 0;
+	int cond;
 	//we need to create all the possible pairs of nodes
 	//So we calculate the number
 	while(i<N){
 		j = 0;
 		while (j<N){
-			int cond;
+			if (i == j){
+				j++;
+				continue;
+			}
 			// if (node_num[i] == 0){
 			// 	cond = (rand()%6);
 			// }
 			// else{
-				cond = (rand()%int(pow(node_num[i]+1,gamma)));
+			cond = (rand()%int(pow(node_num[i]+1,gamma)));
 			// }
 			 
 			if (cond  == 0){
@@ -35,7 +39,7 @@ int main(int argc, char const *argv[])
 			//node_num[i] = rand() / (RAND_MAX + 1.);
 		}
 		//afairw thn tautotikh (i,i) syndesh
-		node_num[i] = node_num[i] -1 ;
+		//node_num[i] = node_num[i] -1 ;
 		i++;
 	}
 	
